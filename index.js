d4885f31a85ed80fd75ea51fe777041f3ce389aa
Mustafa <mireiya@users.noreply.github.com>
2017-07-12 17:49:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#pages").paginate({
-        visiblePage: 10,
-        maxPage: 954,
+        visiblePage: 7,
+        maxPage: 1749,
         onPageChange: function(page) {
             $("#content").html(page);
             console.log(page);
